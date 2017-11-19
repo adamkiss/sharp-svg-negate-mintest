4d778363fc2c73d993b5aad468baa956d14d675c
Adam Kiss <iam@adamkiss.com>
2017-11-19 04:46:12 +0100

    Add more tests

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const sharp = require('sharp')
 
-sharp('./octocat.svg').png().negate().toFile('./octocat.png')
+sharp('./octocat.svg').png().negate().toFile('./octocat-png-negate.png')
+sharp('./octocat.svg').negate().png().toFile('./octocat-negate-png.png')
+sharp('./illustrator.svg').png().negate().toFile('./illustrator-png-negate.png')
+sharp('./illustrator.svg').negate().png().toFile('./illustrator-negate-png.png')
+sharp('./imageoptim.svg').png().negate().toFile('./imageoptim-png-negate.png')
+sharp('./imageoptim.svg').negate().png().toFile('./imageoptim-negate-png.png')
